8c05cf586d63d434117d874edd501d0f9a6eed58
mariabc07 <mariacaburco@hotmail.com>
2023-03-05 00:35:50 -0500

    se agrega el proyecto

---
+++ src/ingenieria_s/hola_mundo.java
@@ -0,0 +1,10 @@
+package ingenieria_s;
+
+public class hola_mundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hola mundo");
+	}
+
+}
---
+++ src/ingenieria_s/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author maria
+ *
+ */
+module ingenieria_s {
+}

48cd337de2bf127eb9ab7cfe9be5511de2ec6259
julianagomez <julianagomez@192.168.20.32>
2023-03-06 21:31:23 -0500

    modificacion

--- src/ingenieria_s/hola_mundo.java
+++ src/ingenieria_s/hola_mundo.java
@@ -5,6 +5,8 @@ public class hola_mundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hola mundo ");
+System.out.println("Modificación de Juliana");
 	}
+	
 
 }
